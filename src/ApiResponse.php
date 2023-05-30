60ca2868afde0519af897558bb8fb515c40fcd64
Hazelint <contact@hazelint.com>
2023-05-30 22:21:36 +0200

    main branch commit

---
+++ src/ApiResponse.php
@@ -0,0 +1,121 @@
+<?php 
+
+namespace Hazelint\Laravel\ApiResponse;
+
+use Illuminate\Http\Response;
+
+class ApiResponse extends Response{
+    // Holds the optional error message (in case of a 'error')
+    protected $errorCode;
+
+    // Holds the optional error message (in case of a 'error')
+    protected $errorMessage;
+
+    // Hold the data
+    protected $data;
+
+
+    /**
+     * Sends content for the current web response.
+     *
+     * @return $this
+     */
+    public function sendContent(): static {
+        $this->header('Content-Type', 'application/json');
+        http_response_code($this->statusCode);
+
+        if($this->getOriginalContent()){
+            $content = $this->morphToJson($this->formatToJsend());
+
+            echo $content;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Formats the response according to the JSEND standard (https://github.com/omniti-labs/jsend)
+     *
+     * @return array|null
+     */
+    public function formatToJsend(){
+        // Create an empty response
+        $response = null;
+
+        if (method_exists($this->data, '__toString') || is_scalar($this->data)){
+            $response_data = $this->data;
+        }else{
+            $response_data = json_encode($this->data);
+        }
+
+        // If response is successful
+        if($this->isSuccessful()){
+            $response = [
+                'status' => 'success',
+                'data' => $response_data,
+            ];
+
+            // If reponse is a client error
+        }elseif ($this->isClientError()){
+            $response = [
+                'status' => 'fail',
+                'data' => $response_data,
+            ];
+
+            // If response is a server error
+        }elseif ($this->isServerError()){
+            $response = [
+                'status' => 'error',
+                'message' => $this->errorMessage,
+            ];
+
+            // Optional error code
+            if($this->errorCode){
+                $response['code'] = $this->errorCode;
+            }
+
+            // Optional data
+            if($this->getOriginalContent()){
+                $response['data'] = $response_data;
+            }
+
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param $errorCode
+     * @return $this
+     */
+    public function setErrorCode($errorCode){
+        $this->errorCode = $errorCode;
+
+        return $this;
+    }
+
+    /**
+     * @param $errorMessage
+     * @return $this
+     */
+    public function setErrorMessage($errorMessage){
+        $this->errorMessage = $errorMessage;
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $content
+     * @param int $status
+     * @param array $headers
+     * @return ApiResponse|void
+     */
+    public static function create($content = '', int $status = 200, array $headers = []){
+        $response = new ApiResponse($content, $status, $headers);
+        $response->data = $content;
+
+        return $response;
+    }
+}
+
+
---
+++ src/ApiResponseFactory.php
@@ -0,0 +1,139 @@
+<?php
+
+
+namespace Hazelint\Laravel\ApiResponse;
+use Hazelint\Laravel\ApiResponse\ApiResponseTypes;
+
+
+class ApiResponseFactory
+{
+    // Success (200 - 299)
+
+    /**
+     * @param $content
+     * @return ApiResponse
+     */
+    static public function success($content = null) {
+        return ApiResponseTypes::success($content);
+    }
+
+    /**
+     * @param $content
+     * @return ApiResponse
+     */
+    static public function created($content = null) {
+        return ApiResponseTypes::success($content, ApiResponseCode::HTTP_SUCCESS_CREATED);
+    }
+
+    /**
+     * @param $content
+     * @return ApiResponse
+     */
+    static public function accepted($content = null) {
+        return ApiResponseTypes::success($content, ApiResponseCode::HTTP_SUCCESS_ACCEPTED);
+    }
+
+    // Redirection (300 - 399)
+
+    /**
+     * @param string $location
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function redirectPermanent(string $location, $content = "Resource has permanently been moved") {
+        return ApiResponseTypes::error($content, ApiResponseCode::HTTP_REDIRECT_PERMANENT)->header('Location', $location);
+    }
+
+    /**
+     * @param string $location
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function redirectTemporary(string $location, $content = "Resource has temporarily been moved") {
+        return ApiResponseTypes::error($content, ApiResponseCode::HTTP_REDIRECT_TEMPORARY)->header('Location', $location);
+    }
+
+    // Client errors (400 - 499)
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function accessDenied($content = 'You are not allowed to use this resource'){
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_ACCESS_DENIED);
+    }
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function loginRequired($content = 'Login is required to use this resource'){
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_LOGIN_REQUIRED);
+    }
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function notFound($content = 'Resource was not found') {
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_NOT_FOUND);
+    }
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function notAcceptable($content = 'The supplied parameters are not acceptable') {
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_NOT_ACCEPTABLE);
+    }
+
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function invalidCredentials($content = 'Credentials are incorrect'){
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_INVALID_CREDENTIALS);
+    }
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function paymentRequired($content = 'Payment required'){
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_PAYMENT_REQUIRED);
+    }
+
+    /**
+     * @param array $validatorErrors
+     * @return ApiResponse
+     */
+    static public function validationError(array $validatorErrors) {
+        return ApiResponseTypes::fail($validatorErrors, ApiResponseCode::HTTP_ERROR_CLIENT_VALIDATION_ERROR);
+    }
+
+    /**
+     * @param string $content
+     * @return ApiResponse
+     */
+    static public function tooManyRequests($content = 'You are sending too many requests'){
+        return ApiResponseTypes::fail($content, ApiResponseCode::HTTP_ERROR_CLIENT_TOO_MANY_REQUESTS);
+    }
+
+    // Server error (500 - 599)
+
+    /**
+     * @param string $error_message
+     * @param int|null $error_code
+     * @return ApiResponse
+     */
+    static public function internalServerError($error_message = 'An internal error has occured', $error_code = null){
+        $response = ApiResponseTypes::error($error_message, ApiResponseCode::HTTP_ERROR_SERVER_INTERNAL);
+
+        if($error_code){
+            $response->setErrorCode($error_code);
+        }
+
+        return $response;
+    }
+}
